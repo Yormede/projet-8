98ff46319da317fa4146ce9fb1b48a515ad004c3
Yormede <ahmedsvg69@gmail.com>
2023-11-09 07:18:25 +0100

    Site finis, Il ne manque que les modifications des images (supprimer les fonds pour avoir des png propres)

--- portfolio/src/components/footer/Footer.js
+++ portfolio/src/components/footer/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './_footer.scss'
 
 const Footer = () => {
     return (
         <footer>
             <div className='footerContainer'>
-                <div className='socialNetwork'></div>
-                <p>Réalisé par Ahmed KHEDIMELLAH</p>
-                <a href='#'>télécharger mon Cv</a>
+                <p className='socialNetwork'>
+                <a href='https://github.com/Yormede' target='_blank' rel="noreferrer"><i className="fa-brands fa-github"></i></a>
+                <a href='https://www.linkedin.com/in/ahmed-khedimellah-aa87991a0/' target='_blank' rel="noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+                </p>
+                <p className='copyright'>Réalisé par Ahmed KHEDIMELLAH</p>
+                <a href='../../images/Cv Ahmed v2.pdf' target="_blank" className='download' rel="noreferrer">Télécharger mon Cv</a>
             </div>
         </footer>
     );
--- portfolio/src/components/realisation/Realisation.js
+++ portfolio/src/components/realisation/Realisation.js
@@ -1,8 +1,28 @@
 import "./_realisations.scss"
 import React, {useState} from 'react';
 import reaOnePic from "../../images/reaOne.png"
+import reaOne1 from "../../images/reaOne1.png"
+import reaOne2 from "../../images/reaOne2.png"
+import reaOne3 from "../../images/reaOne3.png"
+import reaOne4 from "../../images/reaOne4.png"
+import reaOne5 from "../../images/reaOne5.png"
+import reaOne6 from "../../images/reaOne6.png"
+
+
 import reaTwoPic from "../../images/reaTwo.png"
+import reaTwo1 from "../../images/reaTwo1.png"
+import reaTwo2 from "../../images/reaTwo2.png"
+import reaTwo3 from "../../images/reaTwo3.png"
+import reaTwo4 from "../../images/reaTwo4.png"
+import reaTwo5 from "../../images/reaTwo5.png"
+
+
 import reaThreePic from "../../images/reaThree.png"
+import reaThree1 from "../../images/reaThree1.png"
+import reaThree2 from "../../images/reaThree2.png"
+import reaThree4 from "../../images/reaThree4.png"
+import reaThree5 from "../../images/reaThree5.png"
+import reaThree6 from "../../images/reaThree6.png"
 
 const Realisation = () => {
     const [reaOne, setOne] = useState(false);
@@ -30,7 +50,12 @@ const Realisation = () => {
                         <p>Durant ma formation, j'ai du réalisé le coté Frontend d'une application de location immobilière. J'avais accès à tous le design Figma et ai codé le site en React Js</p>
                         <a target="_blank" href="https://github.com/Yormede/Projet-6-OpenClassroom">https://github.com/Yormede/Projet-6-OpenClassroom</a>
                         <div className="imageList">
-
+                            <img src={reaOne1} />
+                            <img src={reaOne2} />
+                            <img src={reaOne3} />
+                            <img src={reaOne4} />
+                            <img src={reaOne5} />
+                            <img src={reaOne6} />
                         </div>
                         <i className="fa-solid fa-chevron-right" onClick={()=>{
                             setTwo(true);
@@ -48,7 +73,11 @@ const Realisation = () => {
                             <p>Durant ma formation, j'ai du réalisé tout le Backend d'un site de notation de livre nommée "Mon Vieux Grimoire". J'avais comme support tous les noms de routes préparer par le chef de projet et les fonctionnalités nécéssaires détaillés. J'ai codé le site à l'aide de NodeJs, ExpressJs et MongoDb Atlas</p>
                             <a target="_blank" href="https://github.com/Yormede/Projet-7">https://github.com/Yormede/Projet-7</a>
                             <div className="imageList">
-
+                                <img src={reaTwo1} />
+                                <img src={reaTwo2} />
+                                <img src={reaTwo3} />
+                                <img src={reaTwo4} />
+                                <img src={reaTwo5} />
                             </div>
                             <i className="fa-solid fa-chevron-right" onClick={()=>{
                                 setThree(true);
@@ -66,7 +95,11 @@ const Realisation = () => {
                         <p>J'ai réalisé à la demande d'un client, le design complet d'un site vitrine pour un service de location de quad et autre véhicule dans le désert de Marrakech au Maroc. J'ai codé le site en React Js et utilisé Figma pour la création du design.</p>
                         <a target="_blank" href="https://quad-yt-buggy.com/">Quad Yt Buggy</a>
                         <div className="imageList">
-
+                            <img src={reaThree1} />
+                            <img src={reaThree2} />
+                            <img src={reaThree4} />
+                            <img src={reaThree5} />
+                            <img src={reaThree6} />
                         </div>
                         <i className="fa-solid fa-chevron-right" onClick={()=>{
                             setOne(true);
